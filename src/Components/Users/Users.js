ee343fb4cfd57ff7428a4a839c1f67b6e94473b2
Kirill Usichenko <kirillusichenko@gmail.com>
2021-05-02 17:23:24 +0300

    get users from firebase

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,37 +1,14 @@
 import React from 'react'
 import s from './Users.module.scss'
+import * as axios from 'axios'
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://s0.rbk.ru/v6_top_pics/resized/1200xH/media/img/5/46/756038770746465.jpg',
-                    followed: false,
-                    fullName: 'Kirill',
-                    status: 'i am a boss',
-                    location: {city: 'Kyiv', country: 'Ukraine'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://s0.rbk.ru/v6_top_pics/resized/1200xH/media/img/5/46/756038770746465.jpg',
-                    followed: true,
-                    fullName: 'Roman',
-                    status: 'i am a boss',
-                    location: {city: 'Kharkiv', country: 'Ukraine'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://s0.rbk.ru/v6_top_pics/resized/1200xH/media/img/5/46/756038770746465.jpg',
-                    followed: false,
-                    fullName: 'Andrey',
-                    status: 'i am a boss',
-                    location: {city: 'Lvyv', country: 'Ukraine'}
-                }
-            ]
+        axios.get('https://social-network-6bc28-default-rtdb.europe-west1.firebasedatabase.app/.json').then(response => {
+                props.setUsers(response.data.items)
+            }
         )
-        console.log(props.users.length)
     }
 
     return (
@@ -40,20 +17,22 @@ const Users = (props) => {
                 props.users.map(user =>
                     <div key={user.id}>
                         <div className={s.userWindow}>
-                            <img src={user.photoUrl} alt="img" className={s.userImg}/>
-                            {user.followed
-                                ? <button onClick={() => {
-                                    props.unfollow(user.id)
-                                }}>Unfollow</button>
-                                : <button onClick={() => {
-                                    props.follow(user.id)
-                                }}>Follow</button>
-                            }
+                            <img src={user.photos.small} alt="img" className={s.userImg}/>
                             <div className={s.userInfo}>
-                                {user.fullName}
-                                {user.status}
-                                {user.location.city}
-                                {user.location.country}
+                                <h1 className={s.userName}>{user.name}</h1>
+                                <div className={s.userLocation}>
+                                    {user.city && `${user.city},`}
+                                    {user.country}
+                                </div>
+                                <div className={s.userStatus}>{user.status}</div>
+                                {user.followed
+                                    ? <button className={s.unfollowButton} onClick={() => {
+                                        props.unfollow(user.id)
+                                    }}>Unfollow</button>
+                                    : <button className={s.followButton} onClick={() => {
+                                        props.follow(user.id)
+                                    }}>Follow</button>
+                                }
                             </div>
                         </div>
                     </div>
--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ import {Provider} from "react-redux";
 
 
     ReactDOM.render(
-        <React.StrictMode>
+
             <BrowserRouter>
                 <Provider store={store}>
                     <App />
                 </Provider>
-            </BrowserRouter>
-        </React.StrictMode>,
+            </BrowserRouter>,
+
         document.getElementById('root')
     );
 
